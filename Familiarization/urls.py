3f3dac9ff63cfc3d6928f2799936af52f556484f
acen20 <ahsen1330@gmail.com>
2021-06-16 13:14:17 +0500

    added url patterns

--- Familiarization/urls.py
+++ Familiarization/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('books/', include('Books.urls')),
     path('admin/', admin.site.urls),
 ]
